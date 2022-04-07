5ab35ff5bb15597d6a176ea31542ace3f78f8dcc
davidrfc <67347740+davidrfc@users.noreply.github.com>
2022-04-06 22:07:22 -0500

    Update action.py

--- rsarl/data/action.py
+++ rsarl/data/action.py
@@ -2,7 +2,9 @@
 from typing import NamedTuple
 
 class Action(NamedTuple):
-    path: list
-    slot_idx: list
+    path1: list
+    path2: list
+    path3: list
+    slot_idx: int
     n_slot: int
     duration: float
